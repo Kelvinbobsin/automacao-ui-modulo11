bd49ae54202815153bcce423e0755135f39ce6c7
Kelvin Bobsin <kelvin.bobsin.1999@hotmail.com>
2023-06-06 21:28:40 -0300

    Exercicio de Automação

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+context('Funcionalidade Login', () => {
+    
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, Aluno')
+    });
+
+    it('Deve exibir mensagem de erro ao inserir usuário inválido', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('kelvin_bobsin@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido.')
+    });
+
+    it('Deve exibir mensagem de erro ao inserir senha inválido', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('kelvin123.com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain', 'Erro: a senha fornecida para o e-mail aluno_ebac@teste.com está incorreta.')
+    });
+});
