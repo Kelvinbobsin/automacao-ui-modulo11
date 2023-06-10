04c05e1a5407ef28ed35b020f1c7401f6b1e00a8
Kelvin Bobsin <kelvin.bobsin.1999@hotmail.com>
2023-06-09 21:35:12 -0300

    Exercicio de Automação

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -1,1 +1,41 @@
 /// <reference types="cypress" />
+
+describe('Funcionalidade Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //.eq(3)
+            .contains('Atlas Fitness Tank')
+            .click()
+    });
+
+    it('Adicionar um produto no carrinho', () => {
+        var quantidade = 4
+
+        cy.get('[class="product-block grid"]')
+            .contains('Atlas Fitness Tank').click()
+        cy.get('.button-variable-item-XS').click()
+        cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Atlas Fitness Tank” foram adicionados no seu carrinho.')
+
+    });
+
+    it.only('Adicionar produto aos favoritos', () => {
+        cy.get('[class="product-block grid"]')
+        .contains('Abominable Hoodie').click()
+        cy.get('.button-variable-item-XL').click()
+        cy.get('.button-variable-item-Red').click()
+        cy.get('.summary > .yith-wcwl-add-to-wishlist > .yith-wcwl-add-button > .add_to_wishlist > :nth-child(2) > span').click()
+        cy.get('#yith-wcwl-message').should('contain' , 'Produto adicionado!')
+    });
+
+});
